2b0f7086dcefdddde5e5df43b3d790947165e678
jeffersonvalente <jeffersonvalente1998@gmail.com>
2022-11-05 15:44:37 -0300

    adicionado a criacao de subredes privadas

--- variable.tf
+++ variable.tf
@@ -14,7 +14,20 @@ variable "enable_dns_support" {
 variable "enable_dns_hostnames" {
         default ="true" 
     }
-#nuemro de subnets a adicionar
+#nuemro de publicas subnets a adicionar
 variable "preferred_number_of_public_subnets" {
-  default = null
+  type = number
+}
+
+#nuemro de privadas subnets a adicionar
+variable "preferred_number_of_private_subnets" {
+  type = number
+}
+variable "name" {
+    type = string
+    
+}
+variable "tags" {
+    type = map(string)
+    default = {}
 }
