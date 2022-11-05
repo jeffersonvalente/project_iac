d6b1a9a0dceb7545bc91300107252fdab111688f
jeffersonvalente <jeffersonvalente1998@gmail.com>
2022-11-05 16:18:59 -0300

    adicionado o route_table.tf

---
+++ route_tables.tf
@@ -0,0 +1,44 @@
+# cria uma tabela de roteamento das subnets privadas
+resource "aws_route_table" "private-rtb" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = format("%s-Private-Route-Table", var.name)
+    },
+  )
+}
+
+# associa todas as subnets privadas a tabela de roteamento privada
+resource "aws_route_table_association" "private-subnets-assoc" {
+  count          = length(aws_subnet.private[*].id)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  route_table_id = aws_route_table.private-rtb.id
+}
+
+# cria uma tabela de roteamento para todas a ssubnet publicas
+resource "aws_route_table" "public-rtb" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = format("%s-Public-Route-Table", var.name)
+    },
+  )
+}
+
+# cria a rota para a tabela de roteamento publico e attach ao IE
+resource "aws_route" "public-rtb-route" {
+  route_table_id         = aws_route_table.public-rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ig.id
+}
+
+# associa todas as subnets publicas a tabela de rotemanto publica
+resource "aws_route_table_association" "public-subnets-assoc" {
+  count          = length(aws_subnet.public[*].id)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public-rtb.id
+}
