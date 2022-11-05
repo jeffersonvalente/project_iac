1fe50706d0df66a13fb9b1f9b4fed65b2cced16d
jeffersonvalente <jeffersonvalente1998@gmail.com>
2022-11-05 13:42:01 -0300

    subnets publicas criadas

--- main.tf
+++ main.tf
@@ -14,3 +14,26 @@ resource "aws_vpc" "main" {
     Name = "projeto_iac"
   }
 }
+# criar subnet publica 10.16.0.0/16 em "us-east-1a"
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.16.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone =  "us-east-1a"
+
+  tags = {
+    Name = "us -east-1a"
+  }
+}
+
+# criar subnet publica 10.16.1.0/16 em "us-east-1a"
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.16.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone =  "us-east-1b"
+
+  tags = {
+    Name = "us-east-1b"
+  }
+}
